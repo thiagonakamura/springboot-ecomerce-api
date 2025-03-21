2fb6cb8754a1bd33d2eaeab852c1597567ac17c2
Thiago Nakamura <thiagokenzo@outlook.com>
2025-03-20 22:13:45 -0300

    Project created

---
+++ src/test/java/com/example/springboot_ecomerce_api/SpringbootEcomerceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_ecomerce_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootEcomerceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
