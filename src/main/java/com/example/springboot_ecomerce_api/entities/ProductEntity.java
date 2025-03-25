bb49eacda8799d1a88504c3be22ef5cd97847c45
Thiago Nakamura <thiagokenzo@outlook.com>
2025-03-24 23:07:04 -0300

    feat: create ProductEntity with JPA annotations

---
+++ src/main/java/com/example/springboot_ecomerce_api/entities/ProductEntity.java
@@ -0,0 +1,107 @@
+package com.example.springboot_ecomerce_api.entities;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+/**
+ * Represents a product entity in the e-commerce system.
+ * This class is mapped to the "tb_product" table in the database.
+ */
+
+@Entity
+@Table(name = "tb_product")
+public class ProductEntity implements Serializable{
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)// Auto-incremented primary key
+	private Long id;
+	private String name;
+	private String description;
+	private Double price;
+	private String imgUrl;
+	
+	private Set<CategoryEntity> categories = new HashSet<>();
+	
+	public ProductEntity() {
+	
+	}
+
+	public ProductEntity(Long id, String name, String description, Double price, String imgUrl) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.description = description;
+		this.price = price;
+		this.imgUrl = imgUrl;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	public Set<CategoryEntity> getCategories() {
+		return categories;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ProductEntity other = (ProductEntity) obj;
+		return Objects.equals(id, other.id);
+	}
+}
