0f907df9077d0d65c69f957a947f1ad569e9c713
Thiago Nakamura <thiagokenzo@outlook.com>
2025-03-25 00:03:33 -0300

    feat: create OrderItemRepository for database operations

--- src/main/java/com/example/springboot_ecomerce_api/repositories/OrderItemRepository.java
+++ src/main/java/com/example/springboot_ecomerce_api/repositories/OrderItemRepository.java
@@ -6,9 +6,9 @@ import com.example.springboot_ecomerce_api.entities.OrderItemEntity;
 import com.example.springboot_ecomerce_api.entities.pk.OrderItemPK;
 
 /**
- * Repository interface for managing OrderEntity persistence.
+ * Repository interface for managing OrderItemEntity persistence.
  * Extends JpaRepository to provide CRUD operations and query capabilities.
  */
-public interface OrderRepository extends JpaRepository<OrderItemEntity, OrderItemPK>{
+public interface OrderItemRepository extends JpaRepository<OrderItemEntity, OrderItemPK>{
 
 }
